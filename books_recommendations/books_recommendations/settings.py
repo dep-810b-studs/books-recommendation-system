542ed9895cd54250709a8580119f8ec1c0b93f7e
Василий Щербаков <v.sherbakov@softpro.com>
2020-12-13 20:47:46 +0300

    add localhost to allowed hosts

--- books_recommendations/books_recommendations/settings.py
+++ books_recommendations/books_recommendations/settings.py
@@ -4,7 +4,11 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'tx)djmti7l58g(+u6^a$cj5$_3=cj&z^l@#=74dcqoa8)jd8e('
 DEBUG = True
-ALLOWED_HOSTS = ['shvasily.pythonanywhere.com']
+ALLOWED_HOSTS = [
+    'shvasily.pythonanywhere.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 INSTALLED_APPS = [
     'django.contrib.auth',
